e1b2e9749d8e99ad5cc7518271355daf441198d1
TheKrystek <krystian@swidurski.pl>
2013-10-31 23:09:22 +0100

    rejestracja, logowanie, wylogowywanie

--- ws/index.php
+++ ws/index.php
@@ -1,5 +1,7 @@
 <?php
 error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 require_once 'mainClass.php';
 require_once 'config.php';
 
@@ -8,30 +10,58 @@ require_once 'config.php';
 	$ws->Connect();
 	if ($ws->isConnected())
 		echo 'Connection has been established!';
+	else
+		echo 'Connection has been not established';
 
+
+	echo '<br/><h1>Users list</h1>';
 	$ws->userPrint();
-	$ws->userNameAlreadyExists('Krystek');
 
+
+	echo '<br/><h1>Users list</h1>';
+	$nick = 'krystek';
+	if ($ws->userNameAlreadyExists($nick))
+		echo "User $nick exists";
+	else
+		echo "User $nick doesn't exist";
 
 	$G = $_GET;
 
 	// 'a' means 'action'
-	if(isset($G['a']))
+	if(isset($_GET['a']))
 	{
 		$action = $G['a'];
-		if ($action == 'register')
+		if ($ws->isLogged())
 		{
-			if (isset($G['user']) && isset($G'pass']) && isset($G['email']))
-				$ws->register()
-					
+			echo "User ".$_SESSION['username']." is currently logged in";
+
+			//Wylogowanie
+			if ($action == 'logout')
+			{	
+				$ws->Logout();
+			}
 
 
 		}
+		else{
+			echo "Nobody is currently logged in";	
 
+			// Rejestracja uzytkownika
+			if ($action == 'register')
+			{
+				if (isset($G['user']) && isset($G['pass']) && isset($G['email']))
+					$ws->register($G['user'],$G['pass'],$G['email']);
 
+			}
 
+			// Logowanie uzytkownika
+			if ($action == 'login')
+			{
+				if (isset($G['user']) && isset($G['pass']))
+					$ws->login($G['user'],$G['pass']);
 
+			}
 
+		}
 	}
-
 ?>
--- ws/mainClass.php
+++ ws/mainClass.php
@@ -9,27 +9,42 @@ class mainClass
 	var $DBHOST;
 	var $DBNAME;
 	var $DBUSER;
-	var $DBPASSWORD;
+	var $DBPASS;
 
 
 
 	public function __construct($user, $password, $host, $database){
 		$this->DBUSER = $user;
-		$this->DBPASSWORD = $password;
+		$this->DBPASS = $password;
 		$this->DBHOST = $host;
 		$this->DBNAME = $database;
+
+		$this->InitializeSession();
 	} 
 
+
+
+	private function InitializeSession(){
+		$session_name = 'myBudgetPal';
+        ini_set('session.use_only_cookies', 1); 
+        $cookieParams = session_get_cookie_params(); 
+        session_set_cookie_params($cookieParams["lifetime"], $cookieParams["path"], $cookieParams["domain"]); 
+        session_name($session_name); 
+        session_start();
+        session_regenerate_id();
+	}
+
 	public function Connect(){
-		$this->mysqli = new mysqli($this->DBHOST, $this->DBUSER, $this->DBPASSWORD, $this->DBNAME);
+		$this->mysqli = new mysqli($this->DBHOST, $this->DBUSER, $this->DBPASS, $this->DBNAME);
+		$this->mysqli->connect_error;
 	}
 
 	public function Close(){
-		$mysqli->close();
+		$this->mysqli->close();
 	}
 
 	public function isConnected(){
-		if ($connection)
+		if (!$this->mysqli->connect_error)
 			return true;
 		else
 			return false;
@@ -37,14 +52,14 @@ class mainClass
 
 
 	public function UserPrint(){
-		$result = $mysqli->query("SELECT * FROM Uzytkownicy");
-	    while ($row = $result->fetch_row()))
-			echo $row['login']." ";
+		$result = $this->mysqli->query("SELECT * FROM Uzytkownicy");
+	    while ($row = $result->fetch_assoc())
+			echo $row['login']."<br/>";
 	}
 
 
 	public function UserNameAlreadyExists($username){	
-		if ($s = $mysqli->prepare("SELECT login,haslo,email FROM Uzytkownicy where login = ?")) { 
+		if ($s = $this->mysqli->prepare("SELECT login FROM Uzytkownicy where login = ?")) { 
 			$s->bind_param('s', $username);
 			$s->execute();
 			$s->store_result();
@@ -56,51 +71,92 @@ class mainClass
 	  	return false;
 	}
 
+
 	public function UserEmailAlreadyExists($email){
-		$result = mysql_query("SELECT * FROM Uzytkownicy where email = '$email'");		
-		if (mysql_num_rows($result) > 0)
-			return true;
-		else 
-			return false;
+		if ($s = $this->mysqli->prepare("SELECT email FROM Uzytkownicy where email = ?")) { 
+			$s->bind_param('s', $email);
+			$s->execute();
+			$s->store_result();
+			if ($s->num_rows == 1)
+				return true;
+			else
+				return false;
+	  	}
+	  	return false;
 	}
 
 
-	public function Register($USER, $password, $email){
-		if (!$this->USERNameAlreadyExists($USER)){
-			if (!thist->USEREmailAlreadyExists($email)){
-				echo 'USER has been registered';
-
+	public function Register($login, $password, $email){
+		if (!$this->UserNameAlreadyExists($login)){
+			if (!$this->UserEmailAlreadyExists($email)){
+				if ($s = $this->mysqli->prepare("INSERT INTO Uzytkownicy (login, haslo, email, ip, userAgent, potwierdzony) values (?, ?, ?, ?, ?, 0)")) { 
+					$password = hash('sha256',$password);
+					$s->bind_param('sssss', $login,$password,$email,$_SERVER['REMOTE_ADDR'],$_SERVER['HTTP_USER_AGENT']);
+					$s->execute();
+					$s->store_result();
+	  			}
 			}
 			else
 				echo 'This email is already taken!';
 		}	
 		else
-			echo 'This USERname is already taken!';
+			echo 'This username is already taken!';
 	}
 
 
+	public function Login($login, $password)
+	{
+		if ($this->UserNameAlreadyExists($login)){
+			if ($s = $this->mysqli->prepare("SELECT ID_Uzytkownika, login, haslo FROM Uzytkownicy where login = ? AND haslo = ?")) { 
+				$s->bind_param('ss', $login,$password);
+				$s->execute();
+				$s->store_result();
+				$s->bind_result($userId, $username, $password);
+				$s->fetch();
+				if ($s->num_rows == 1){
+					// Poprawnie zalogowano
+					$user_browser = $_SERVER['HTTP_USER_AGENT']; 
+					$_SESSION['userId'] = $userId; 
+					$_SESSION['username'] = $username;
+					$_SESSION['login_string'] = hash('sha512', $password.$user_browser);	
+				}
+  			}
+		}	
+		else
+			echo 'Username or password is wrong';
+	}
 
 
+	public function isLogged(){
+		if(isset($_SESSION['userId'], $_SESSION['username'], $_SESSION['login_string'])) {
+     		$userId = $_SESSION['userId'];
+     		$login_string = $_SESSION['login_string'];
+     		$username = $_SESSION['username'];
+     		$user_browser = $_SERVER['HTTP_USER_AGENT']; 
+     		if ($s = $this->mysqli->prepare("SELECT haslo FROM Uzytkownicy WHERE ID_Uzytkownika = ? LIMIT 1")) { 
+		        $s->bind_param('i', $userId);
+		        $s->execute(); 
+		        $s->store_result();
+		        if($s->num_rows == 1) { 
+		           $s->bind_result($password);
+		           $s->fetch();
+		           $login_check = hash('sha512', $password.$user_browser);
+		           if($login_check == $login_string) {		              
+		              return true;
+		           }
+		       }
+		   }
+		}
+		return false;
+	}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	public function Logout(){
+		$_SESSION = array();
+		$params = session_get_cookie_params();
+		setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"]);
+		session_destroy();
+	}
 
 }
-
-
 ?>
