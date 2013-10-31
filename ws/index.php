2cdcaae822fcc9c25b50ab117941a171e3c85599
TheKrystek <krystian@swidurski.pl>
2013-10-31 21:06:42 +0100

    czy uzytkownik istnieje

--- ws/index.php
+++ ws/index.php
@@ -1,4 +1,37 @@
 <?php
+error_reporting(E_ALL);
+require_once 'mainClass.php';
+require_once 'config.php';
 
+
+	$ws = new mainClass($user,$pass,$host,$base);
+	$ws->Connect();
+	if ($ws->isConnected())
+		echo 'Connection has been established!';
+
+	$ws->userPrint();
+	$ws->userNameAlreadyExists('Krystek');
+
+
+	$G = $_GET;
+
+	// 'a' means 'action'
+	if(isset($G['a']))
+	{
+		$action = $G['a'];
+		if ($action == 'register')
+		{
+			if (isset($G['user']) && isset($G'pass']) && isset($G['email']))
+				$ws->register()
+					
+
+
+		}
+
+
+
+
+
+	}
 
 ?>
--- ws/mainClass.php
+++ ws/mainClass.php
@@ -4,33 +4,28 @@
 class mainClass
 {
 
-	var $connection;
+	var $mysqli;
 
-	var $host;
-	var $database;
-
-	var $user;
-	var $password;
+	var $DBHOST;
+	var $DBNAME;
+	var $DBUSER;
+	var $DBPASSWORD;
 
 
 
 	public function __construct($user, $password, $host, $database){
-		$this->user = $user;
-		$this->password = $password;
-		$this->host = $host;
-		$this->database = $database;
+		$this->DBUSER = $user;
+		$this->DBPASSWORD = $password;
+		$this->DBHOST = $host;
+		$this->DBNAME = $database;
 	} 
 
 	public function Connect(){
-		$this->connection = mysql_connect($this->host, $this->database, $this->password);
-		if (!$this->connection) 
-    		die('Could not connect: ' . mysql_error());
-    	else
-    		mysql_select_db($this->database);
+		$this->mysqli = new mysqli($this->DBHOST, $this->DBUSER, $this->DBPASSWORD, $this->DBNAME);
 	}
 
 	public function Close(){
-		mysql_close($this->connection);
+		$mysqli->close();
 	}
 
 	public function isConnected(){
@@ -39,6 +34,71 @@ class mainClass
 		else
 			return false;
 	}
+
+
+	public function UserPrint(){
+		$result = $mysqli->query("SELECT * FROM Uzytkownicy");
+	    while ($row = $result->fetch_row()))
+			echo $row['login']." ";
+	}
+
+
+	public function UserNameAlreadyExists($username){	
+		if ($s = $mysqli->prepare("SELECT login,haslo,email FROM Uzytkownicy where login = ?")) { 
+			$s->bind_param('s', $username);
+			$s->execute();
+			$s->store_result();
+			if ($s->num_rows == 1)
+				return true;
+			else
+				return false;
+	  	}
+	  	return false;
+	}
+
+	public function UserEmailAlreadyExists($email){
+		$result = mysql_query("SELECT * FROM Uzytkownicy where email = '$email'");		
+		if (mysql_num_rows($result) > 0)
+			return true;
+		else 
+			return false;
+	}
+
+
+	public function Register($USER, $password, $email){
+		if (!$this->USERNameAlreadyExists($USER)){
+			if (!thist->USEREmailAlreadyExists($email)){
+				echo 'USER has been registered';
+
+			}
+			else
+				echo 'This email is already taken!';
+		}	
+		else
+			echo 'This USERname is already taken!';
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
