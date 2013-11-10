5de7e5cc3f709d9840b9165af81c13398461d702
TheKrystek <krystian@swidurski.pl>
2013-11-10 22:48:43 +0100

    zmiana strony glownej

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-﻿<title>Planowanie budżetu domowego - My Budget Pal</title>
+﻿<title>Home budget planning - My Budget Pal</title>
 
 <meta name="keywords" content="money, budget, home budget planner, planowanie budżetu, planowanie finansów, kontrola wydatków firmy, skaner paragonów, świdurski, kasprzak, krystian świdurski, tomasz kasprzak, tomek kasprzak" />
-<meta name="description" content="Na co wydajesz swoje pieniądze? Kieruj swoimi wydatkami z My Budget Pal" />
+<meta name="description" content="How do you spend your money? Manage your expenses with  My Budget Pal" />
 
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
@@ -44,8 +44,9 @@ a:hover
 
 <center><img src="budget_logo_320.png" alt="jak planować budżet" title="budget planner"><br />
 <h1>MyBudgetPal.com</h1>
-<h2>zaplanuj budżet dla domu i firmy</h2>
-<h3>wersja webowa, aplikacja Android z systemem OCR paragonów fiskalnych</h3>
+<h2>home budget planning made easy</h2>
+<h3>webservice + android application with OCR module</h3>
 <p>coming soon...</p>
+<p>developed as student project</p>
 <p><a href="ws/">api</a></p>
 </center>
